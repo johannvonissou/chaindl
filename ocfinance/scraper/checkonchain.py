d255ce62f9d4747121bb37d55268b1b969dc24d4
Dhruvan Gnanadhandayuthapani <dhruvan2006@gmail.com>
2024-10-12 18:59:50 +0200

    CheckOnChain manage mixed date formats

--- ocfinance/scraper/checkonchain.py
+++ ocfinance/scraper/checkonchain.py
@@ -15,7 +15,7 @@ def _download(url):
 
     merged_df = pd.concat(dfs, axis=1, join='outer')
     return merged_df
-    
+
 def _extract_data_from_scripts(scripts):
     dfs = []
     for script in scripts:
@@ -27,9 +27,9 @@ def _extract_data_from_scripts(scripts):
                 x = json.loads(x_data)
                 y = json.loads(y_data)
 
-                df = pd.DataFrame({ name: pd.to_numeric(y, errors='coerce') }, index=pd.to_datetime(pd.to_datetime(x).date))
+                df = pd.DataFrame({ name: pd.to_numeric(y, errors='coerce') }, index=pd.to_datetime(pd.to_datetime(x, format='mixed').date))
                 df.index.name = 'Date'
                 df = df.loc[~df.index.duplicated(keep='first')] # TODO: Give user option to either choose drop dupes or take avg
                 dfs.append(df)
-    
+
     return dfs

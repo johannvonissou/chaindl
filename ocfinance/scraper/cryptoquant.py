b2a056a02ca9cb5be8002d052bee855f4b03437a
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-10-21 22:22:46 +0200

    Replace seleniumbase with selenium for cryptoquant along with remote browser proxy

--- ocfinance/scraper/cryptoquant.py
+++ ocfinance/scraper/cryptoquant.py
@@ -1,6 +1,7 @@
+import time
 import json
 from urllib.parse import urlparse
-from seleniumbase import SB
+from selenium import webdriver
 import pandas as pd
 
 CRYPTOQUANT_URL = "https://cryptoquant.com/"
@@ -13,11 +14,14 @@ def _download(url, **kwargs):
     if splits[1] == 'asset':
         raise NotImplementedError("Only third party metrics on cryptoquant have been implemented.")
 
+    proxy = kwargs.get('proxy', None)
+    driver = _get_driver(proxy=proxy)
+
     email = kwargs.get('email')
     password = kwargs.get('password')
     if not email or not password:
         raise TypeError("Email and/or password hasn't been passed")
-    data = _get_json(id, email, password)
+    data = _get_json(driver, id, email, password)
 
     columns = data['data']['result']['columns']
     results = data['data']['result']['results']
@@ -43,56 +47,54 @@ def _create_dataframe(results, column_names):
     
     return df
 
-def _get_json(id, email, password):
-    with SB(uc=True) as sb:
-        sb.uc_open_with_reconnect(CRYPTOQUANT_URL, 4)
-        sb.uc_gui_click_captcha()
+def _get_driver(proxy=None):
+    chrome_options = webdriver.ChromeOptions()
 
-        response = sb.execute_async_script(f"""
-            var callback = arguments[arguments.length - 1];
-            
-            fetch("https://live-api.cryptoquant.com/api/v1/sign-in", {{
-                method: 'POST',
-                headers: {{
-                    'Content-Type': 'application/json'
-                }},
-                body: JSON.stringify({{
-                    "email": "{email}",
-                    "password": "{password}"
-                }})
-            }}).then(response => {{
-                console.log('Response received:', response);
-                return response.json();
-            }})
-            .then(data => {{
-                console.log('Data parsed:', data);
-                callback(data);
-            }})
-            .catch(err => {{
-                console.log('Error occurred:', err);
-                callback({{'error': err.toString()}});
-            }});
-        """)
-
-        if 'accessToken' in response:
-            access_token = response['accessToken']
-
-            data_url = f"https://live-api.cryptoquant.com/api/v1/analytics/{id}"
-            
-            result = sb.execute_async_script(f"""
-                var done = arguments[0];
-                
-                fetch("{data_url}", {{
-                    method: 'GET',
-                    headers: {{
-                        'Authorization': 'Bearer {access_token}',
-                        'Accept': 'application/json'
-                    }}
-                }}).then(response => response.text())
-                .then(data => done(data))
-                .catch(err => done({{'error': err.toString()}}));
-            """)
-        else:
-            print(f"Error occurred: {response.get('error')}")
+    if proxy:
+        driver = webdriver.Remote(proxy, options=chrome_options)
+    else:
+        driver = webdriver.Chrome(options=chrome_options)
     
-    return json.loads(result)
+    return driver
+
+def _get_json(driver, id, email, password):
+    driver.get(CRYPTOQUANT_URL)
+    time.sleep(4)
+
+    # Execute the login request
+    script = f"""
+        return fetch("https://live-api.cryptoquant.com/api/v1/sign-in", {{
+            method: 'POST',
+            headers: {{
+                'Content-Type': 'application/json'
+            }},
+            body: JSON.stringify({{
+                "email": "{email}",
+                "password": "{password}"
+            }})
+        }}).then(response => response.json());
+    """
+    response = driver.execute_script(script)
+
+    if 'accessToken' in response:
+        access_token = response['accessToken']
+
+        data_url = f"https://live-api.cryptoquant.com/api/v1/analytics/{id}"
+        
+        result_script = f"""
+            return fetch("{data_url}", {{
+                method: 'GET',
+                headers: {{
+                    'Authorization': 'Bearer {access_token}',
+                    'Accept': 'application/json'
+                }}
+            }}).then(response => response.json());
+        """
+        result = driver.execute_script(result_script)
+    else:
+        print(f"Error occurred: {response.get('error')}")
+        driver.quit()
+        return {}
+
+    driver.quit()
+    return result
